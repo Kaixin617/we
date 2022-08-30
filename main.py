89996f915890cc81b60dc04c8c42fdb93eb390b6
Kaixin617 <112503156+Kaixin617@users.noreply.github.com>
2022-08-30 18:35:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
     # -*- coding: utf-8 -*-
 
 conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
-params = urllib.parse.urlencode({'key':'你的APIKEY'})
+params = urllib.parse.urlencode({'key':'f9343f97e7964c577995ce119b5ffbc3'})
 headers = {'Content-type':'application/x-www-form-urlencoded'}
 conn.request('POST','/qingshi/index',params,headers)
 res = conn.getresponse()
